939504831b805b3d84d799310908c73bb4e14d8d
Almada2021 <87843281+Almada2021@users.noreply.github.com>
2023-03-18 17:32:05 +0000

    eature: Layout Set up

---
+++ twitter-clone/pages/index.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <div className="text-3xl text-sky-500">
+      <Header showBackArrow={true} label="Home"/>
+    </div>
+  )
+}
