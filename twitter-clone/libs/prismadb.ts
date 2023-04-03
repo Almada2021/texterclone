88cbeb5e32fef4178a489c329b67588e4a4c6547
Almada2021 <87843281+Almada2021@users.noreply.github.com>
2023-04-03 06:35:13 +0000

    Revert "update for deployment"
    This reverts commit 20c8769c15cd130959d12670b34d57a81a736978.

--- twitter-clone/libs/prismadb.ts
+++ twitter-clone/libs/prismadb.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient;
 }
 // prevent hot reloads multyples instances
 const client = globalThis.prisma || new PrismaClient();
