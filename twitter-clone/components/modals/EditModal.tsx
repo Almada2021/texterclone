abfda4fa6d70e187e11514cc4ef266e0435f39e6
Almada2021 <87843281+Almada2021@users.noreply.github.com>
2023-04-03 06:24:09 +0000

    fix: error with patch method

--- twitter-clone/components/modals/EditModal.tsx
+++ twitter-clone/components/modals/EditModal.tsx
@@ -13,11 +13,11 @@ const EditModal = () => {
   const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
   const editModal = useEditModal();
 
-  const [profileImage, setProfileImage] = useState("");
-  const [coverImage, setCoverImage] = useState("");
-  const [name, setName] = useState("");
-  const [username, setUserName] = useState("");
-  const [bio, setBio] = useState("");
+  const [profileImage, setProfileImage] = useState(" ");
+  const [coverImage, setCoverImage] = useState(" ");
+  const [name, setName] = useState(" ");
+  const [username, setUserName] = useState(" ");
+  const [bio, setBio] = useState(" ");
   useEffect(() => {
     setProfileImage(currentUser?.profileImage);
     setCoverImage(currentUser?.coverImage);
@@ -39,6 +39,7 @@ const EditModal = () => {
       mutateFetchedUser();
       toast.success("updated");
     } catch (error) {
+      console.log(error);
       toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
@@ -55,38 +56,38 @@ const EditModal = () => {
   ]);
   const bodyContent = (
     <div className="flex flex-col gap-4">
-        <ImageUpload
-            value={profileImage}
-            disabled={isLoading}
-            onChange={(image) => setProfileImage(image)}
-            label="Upload Profile Image"
-        />
-        <ImageUpload
-            value={coverImage}
-            disabled={isLoading}
-            onChange={(image) => setCoverImage(image)}
-            label="Upload Profile Image"
-        />
-        <Input
-            placeholder="Name"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            disabled={isLoading}
-        />
-        <Input
-            placeholder="Usename"
-            onChange={(e) => setUserName(e.target.value)}
-            value={username}
-            disabled={isLoading}
-        />
-        <Input
-            placeholder="Bio"
-            onChange={(e) => setBio(e.target.value)}
-            value={bio}
-            disabled={isLoading}
-        />
+      <ImageUpload
+        value={profileImage}
+        disabled={isLoading}
+        onChange={(image) => setProfileImage(image)}
+        label="Upload Profile Image"
+      />
+      <ImageUpload
+        value={coverImage}
+        disabled={isLoading}
+        onChange={(image) => setCoverImage(image)}
+        label="Upload Profile Image"
+      />
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Usename"
+        onChange={(e) => setUserName(e.target.value)}
+        value={username}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Bio"
+        onChange={(e) => setBio(e.target.value)}
+        value={bio}
+        disabled={isLoading}
+      />
     </div>
-  )
+  );
   return (
     <Modal
       disabled={isLoading}
